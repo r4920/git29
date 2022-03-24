26de3196285f7e9a93b3eaf51e1c29a1f970e96d
r4920 <ankitpatel@qable.io>
2022-03-24 12:10:18 +0000

    Code Synced by DhiWise

---
+++ app/src/main/java/com/ankitsapplication/app/modules/appnavigation/ui/AppNavigationActivity.kt
@@ -0,0 +1,56 @@
+package com.ankitsapplication.app.modules.appnavigation.ui
+
+import androidx.activity.viewModels
+import com.ankitsapplication.app.R
+import com.ankitsapplication.app.appcomponents.base.BaseActivity
+import com.ankitsapplication.app.databinding.ActivityAppNavigationBinding
+import com.ankitsapplication.app.modules.appnavigation.`data`.viewmodel.AppNavigationVM
+import com.ankitsapplication.app.modules.incomingcallinthemiddleofanothercall.ui.IncomingCallInTheMiddleOfAnotherCallActivity
+import com.ankitsapplication.app.modules.incomingcallnotification.ui.IncomingCallNotificationActivity
+import com.ankitsapplication.app.modules.incomingcallnotification3.ui.IncomingCallNotification3Activity
+import com.ankitsapplication.app.modules.incomingcallnotificationmorethanoneprofile.ui.IncomingCallNotificationMoreThanOneProfileActivity
+import com.ankitsapplication.app.modules.incominggroupcallnotification.ui.IncomingGroupCallNotificationActivity
+import com.ankitsapplication.app.modules.telegramxmainscreen.ui.TelegramXMainScreenActivity
+import kotlin.String
+import kotlin.Unit
+
+public class AppNavigationActivity :
+    BaseActivity<ActivityAppNavigationBinding>(R.layout.activity_app_navigation) {
+  private val viewModel: AppNavigationVM by viewModels<AppNavigationVM>()
+
+  public override fun onInitialized(): Unit {
+    viewModel.navArguments = intent.extras
+    binding.appNavigationVM = viewModel
+  }
+
+  public override fun setUpClicks(): Unit {
+    binding.linear06IncomingCallInTheMiddleOfAnotherCall.setOnClickListener {
+      val destIntent = IncomingCallInTheMiddleOfAnotherCallActivity.getIntent(this, null)
+      startActivity(destIntent)
+    }
+    binding.linear05IncomingGroupCallNotification.setOnClickListener {
+      val destIntent = IncomingGroupCallNotificationActivity.getIntent(this, null)
+      startActivity(destIntent)
+    }
+    binding.linear02IncomingCallNotification.setOnClickListener {
+      val destIntent = IncomingCallNotificationActivity.getIntent(this, null)
+      startActivity(destIntent)
+    }
+    binding.linear01TelegramXMainScreen.setOnClickListener {
+      val destIntent = TelegramXMainScreenActivity.getIntent(this, null)
+      startActivity(destIntent)
+    }
+    binding.linear04IncomingCallNotificationMoreThanOneProfile.setOnClickListener {
+      val destIntent = IncomingCallNotificationMoreThanOneProfileActivity.getIntent(this, null)
+      startActivity(destIntent)
+    }
+    binding.linear03IncomingCallNotification.setOnClickListener {
+      val destIntent = IncomingCallNotification3Activity.getIntent(this, null)
+      startActivity(destIntent)
+    }
+  }
+
+  public companion object {
+    public const val TAG: String = "APP_NAVIGATION_ACTIVITY"
+  }
+}
---
+++ app/src/main/java/com/ankitsapplication/app/modules/incomingcallnotification/data/model/IncomingCallNotification2RowModel.kt
@@ -0,0 +1,29 @@
+package com.ankitsapplication.app.modules.incomingcallnotification.`data`.model
+
+import com.ankitsapplication.app.R
+import com.ankitsapplication.app.appcomponents.di.MyApp
+import kotlin.String
+
+public data class IncomingCallNotification2RowModel(
+  /**
+   * TODO Replace with dynamic value
+   */
+  public var txtRobertLeeZeme: String? =
+      MyApp.getInstance().resources.getString(R.string.msg_robert_lee_zeme)
+  ,
+  /**
+   * TODO Replace with dynamic value
+   */
+  public var txtDirector: String? = MyApp.getInstance().resources.getString(R.string.lbl_director)
+  ,
+  /**
+   * TODO Replace with dynamic value
+   */
+  public var txtMaybe: String? = MyApp.getInstance().resources.getString(R.string.lbl_maybe)
+  ,
+  /**
+   * TODO Replace with dynamic value
+   */
+  public var txtMay02: String? = MyApp.getInstance().resources.getString(R.string.lbl_may_02)
+
+)
